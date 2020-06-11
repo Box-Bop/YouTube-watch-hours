5b9c17e306a62aa0416fe68eee4f6d566ee0af60
Robert <tohverrobert@gmail.com>
2020-06-12 00:06:04 +0300

    formatting and Express

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const axios = require('axios');
 var express = require("express");
 const moment = require('moment');
 
-const watchHistory = require('../watch-history.json');
+const watchHistoryTemp = require('../watch-history.json');
 const apiKey = require('./yt-api-key.js');
 
 const apiURL = `https://www.googleapis.com/youtube/v3/videos?part=contentDetails&key=${apiKey.key}&id=`;
-const excludedMinutes = 180;
+let excludedMinutes = 0;
 
 let videoIds = '';
 let idAmount = 0;
@@ -20,25 +20,25 @@ async function makeRequest(ids) {
 	await axios.get(`${apiURL}${ids}`).then(response => {
 		response.data.items.forEach(item => {
 			totalVidCount++;
-			
+
 			let duration = item.contentDetails.duration.split(/[THMS]/);
 			duration = duration.slice(1, -1); // Removes array elements which RegEX split doesn't remove
-			
+
 			const seconds = duration[duration.length - 1];
 			const minutes = duration[duration.length - 2] && duration[duration.length - 2];
 			const hours = duration[duration.length - 3] && duration[duration.length - 3];
-			
+
 			const tempTime = moment.duration(0, 'minutes');
-			
+
 			seconds ? tempTime.add(parseInt(seconds), 'seconds') : {};
 			minutes ? tempTime.add(parseInt(minutes), 'minutes') : {};
 			hours ? tempTime.add(parseInt(hours), 'hours') : {};
-			
-			if (tempTime.as('minutes') >= excludedMinutes) {
+
+			if (excludedMinutes && tempTime.as('minutes') >= excludedMinutes) {
 				excludedVids++;
 				return;
 			}
-			
+
 			seconds ? totalSeconds.add(parseInt(seconds), 'seconds') : {};
 			minutes ? totalSeconds.add(parseInt(minutes), 'minutes') : {};
 			hours ? totalSeconds.add(parseInt(hours), 'hours') : {};
@@ -48,23 +48,23 @@ async function makeRequest(ids) {
 	});
 }
 
-async function start() {
+async function start(watchHistory) {
 	for (vid of watchHistory) {
 		if (vid.titleUrl) {
-			
+
 			if (idAmount == 50 || watchHistory.indexOf(vid) == watchHistory.length - 1) {
 				videoIds = videoIds.slice(0, -1);
 				await makeRequest(videoIds);
-				console.log(totalVidCount);
+				console.log('Videos requested:', totalVidCount);
 				idAmount = 0;
 				videoIds = '';
 				continue;
 			}
-			
+
 			vidID = vid.titleUrl.split('=')[1];
 			videoIds += `${vidID},`;
 			idAmount++;
-			
+
 		} else {
 			deletedVids++;
 		}
@@ -74,13 +74,41 @@ async function start() {
 
 	console.log(`Total watchtime: ${timeData.seconds} seconds, ${timeData.minutes} minutes, ${timeData.hours} hours, ${timeData.days} days and ${timeData.months} months`);
 	console.log(`Total amount of videos watched: ${totalVidCount}`);
-	
+
 	const asSeconds = totalSeconds.as('seconds');
 	const average = asSeconds / totalVidCount;
-	
+
 	console.log(`Average video lenght: ${moment.utc(moment.duration(average, 'seconds').as('seconds')).format('HH:mm:ss')}`);
 	console.log(`Amount of videos longer than ${excludedMinutes} minutes: ${excludedVids}\nDeleted videos: ${deletedVids}`);
+
+	return {
+		totalWatchTime: {
+			seconds: timeData.seconds,
+			minutes: timeData.minutes,
+			hours: timeData.hours,
+			days: timeData.days,
+			months: timeData.months
+		},
+		otherStats: {
+			videosWatched: totalVidCount,
+			avgVideoLenght: moment.utc(moment.duration(average, 'seconds').as('seconds')).format('HH:mm:ss'),
+			longerThanLimit: excludedVids,
+			deletedVideos: deletedVids
+		},
+	}
 }
 
 var app = express();
-start();
+app.use(express.json({ limit: '50mb' })); // lets you get request JSON
+
+app.listen(3000, () => {
+	console.log("Server running on port 3000");
+});
+
+app.post("/get-stats/:limit?", async (req, res) => {
+	console.log('started');
+	const limitParam = parseInt(req.params.limit);
+	req.params.limit && !isNaN(limitParam) ? excludedMinutes = limitParam : {};
+	const responseData = await start(req.body);
+	res.send(responseData);
+});
